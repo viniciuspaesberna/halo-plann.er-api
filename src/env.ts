a971978d330031cf928ff87e4ab5c39313205df4
Vinicius Paes Berna <viniciuspaesberna@gmail.com>
2024-12-16 19:07:39 -0300

    make nodemailer to use env const

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,8 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   API_BASE_URL: z.string().url(),
   WEB_BASE_URL: z.string().url(),
+  GOOGLE_APP_USER: z.string(),
+  GOOGLE_APP_PASSWORD: z.string(),
   PORT: z.coerce.number().default(3333),
 })
 
--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,40 +1,37 @@
 import nodemailer from 'nodemailer'
 
-export async function getMailClient() {
-  const account = await nodemailer.createTestAccount()
+import { env } from '../env'
 
+// export async function getMailClient() {
+//   const account = await nodemailer.createTestAccount()
+
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: account.user,
+//       pass: account.pass,
+//     },
+//   })
+
+//   return transporter
+// }
+
+const googleAppUser = env.GOOGLE_APP_USER
+const googleAppPassword = env.GOOGLE_APP_PASSWORD
+
+export async function getMailClient() {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
+    service: 'plann.er',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
-      user: account.user,
-      pass: account.pass,
+      user: googleAppUser,
+      pass: googleAppPassword,
     },
   })
 
   return transporter
 }
-
-// import dotenv from 'dotenv'
-// import nodemailer from 'nodemailer'
-
-// dotenv.config()
-
-// const googleAppUser = process.env.GOOGLE_APP_USER
-// const googleAppPassword = process.env.GOOGLE_APP_PASSWORD
-
-// export async function getMailClient() {
-//   const transporter = nodemailer.createTransport({
-//     service: 'plann.er',
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: googleAppUser,
-//       pass: googleAppPassword
-//     }
-//   })
-
-//   return transporter
-// }
